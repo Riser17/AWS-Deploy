b1c81229d73e9aa74d2ba923836a916f90607bfc
Riser17 <91198103+Riser17@users.noreply.github.com>
2023-02-03 10:22:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: {"/api": "http://localhost:9090/" },
+    proxy: {"/api": "https://server-cv.vercel.app/" },
   },
   plugins: [react()],
 })
